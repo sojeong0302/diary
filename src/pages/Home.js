fbf556b46fc31a41174ac1ad2273c1918c017656
sojeong0302 <01duqrl@naver.com>
2023-12-18 22:11:11 +0900

    feat : 공통 컴포넌트 (Button)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import { useSearchParams } from "react-router-dom";
+import Button from "../component/Button.js";
 
 const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams.get("sort"));
 
-  return <div className="Home">Home 페이지입니다.</div>;
+  return (
+    <div className="Home">
+      <Button text={"기본 버튼"} />
+      <Button type="positive" text={"긍정 버튼"} />
+      <Button type="negative" text={"부정 버튼"} />
+    </div>
+  );
 };
 
 export default Home;
