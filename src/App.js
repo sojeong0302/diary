75e8a1fe8e517bf6fef869f8c66a09b9dd390a16
sojeong0302 <01duqrl@naver.com>
2023-12-18 21:06:06 +0900

    feat : 동적 경로 라우팅

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="/edit" element={<Edit />} />
       </Routes>
       <div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,9 @@
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("sort"));
+
   return <div className="Home">Home 페이지입니다.</div>;
 };
 
