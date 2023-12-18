dbbc7e440144d98a2ee864289e0b716f2eeefa55
sojeong0302 <01duqrl@naver.com>
2023-12-18 22:57:17 +0900

    feat : 공통 컴포넌트(Header)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,26 @@
-import { useSearchParams } from "react-router-dom";
 import Button from "../component/Button.js";
+import Header from "../component/Header.js";
 
 const Home = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
   return (
-    <div className="Home">
-      <Button text={"기본 버튼"} />
-      <Button type="positive" text={"긍정 버튼"} />
-      <Button type="negative" text={"부정 버튼"} />
+    <div>
+      <Header
+        title={"Home"}
+        leftChild={
+          <Button
+            type="positive"
+            text={"긍정 버튼"}
+            onClick={alert("positive button ")}
+          />
+        }
+        rightChild={
+          <Button
+            type="negative"
+            text={"부정 버튼"}
+            onClick={alert("negative button")}
+          />
+        }
+      />
     </div>
   );
 };
