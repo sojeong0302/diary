727846dc7df8251315614703061b84ab3d82fc05
sojeong0302 <01duqrl@naver.com>
2023-12-21 16:50:21 +0900

    style : 선택한 감정 이미지 하이라이트

--- src/component/EmotionItem.js
+++ src/component/EmotionItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./EmotionItem.css";
 
-const EmotionItem = ({ id, img, name, onClick, insSelected }) => {
+const EmotionItem = ({ id, img, name, onClick, isSelected }) => {
   const handleOnClick = () => {
     onClick(id);
   };
   return (
-    <div className="EmotionItem" onClick={handleOnClick}>
+    <div
+      className={[
+        "EmotionItem",
+        isSelected ? `EmotionItem_on_${id}` : `EmotionItem_off`,
+      ].join(" ")}
+      onClick={handleOnClick}
+    >
       <img alt={`emotion${id}`} src={img} />
       <span>{name}</span>
     </div>
