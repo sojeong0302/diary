71e4cf638199f0fc24051242c23c633d38069c4e
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-12-28 15:52:31 +0900

    feat : 상단부 정렬 기능 구현

--- src/component/DiaryList.js
+++ src/component/DiaryList.js
@@ -1,12 +1,30 @@
 import "./DiaryList.css";
 import Button from "./Button";
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
 
 const DiaryList = ({ data }) => {
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
   return (
     <div className="DiaryList">
       <div className="menu_wrapper">
         <div className="left_col">
-          <select></select>
+          <select value={sortType} onChange={onChangeSortType}>
+            {sortOptionList.map((it, idx) => (
+              <option key={idx} value={it.value}>
+                {it.name}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="right_col">
           <Button type={"positive"} text={"새 일기 쓰기"} />
