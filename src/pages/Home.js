4fc8bc1cbbb81c9f1e6f58cf6f6efbedee0242eb
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-12-28 15:16:45 +0900

    feat : Home Header 화살표에 따라 날짜 변경 기능 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import Button from "../component/Button";
+import Header from "../component/Header";
+
 const Home = () => {
-  return <div>Home 페이지입니다.</div>;
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const headerTitle = `${pivotDate.getFullYear()}년 ${
+    pivotDate.getMonth() + 1
+  }월`;
+
+  const onIncreaseMoth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
+  const onDecreaseMoth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  return (
+    <div>
+      <Header
+        title={headerTitle}
+        leftChild={<Button text={"<"} onClick={onDecreaseMoth} />}
+        rightChild={<Button text={">"} onClick={onIncreaseMoth} />}
+      />
+    </div>
+  );
 };
 
 export default Home;
