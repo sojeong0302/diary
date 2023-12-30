eb11f02db46dea9f5a862e509f754dec964d7f21
sojeong0302 <01duqrl@naver.com>
2023-12-31 05:27:59 +0900

    feat : Edit 헤더 구현

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,14 +1,45 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useDiary from "../hooks/useDiary";
+import Button from "../component/Button";
+import Header from "../component/Header";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const Edit = () => {
   const { id } = useParams();
   const data = useDiary(id);
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const { onDelete } = useContext(DiaryDispatchContext);
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      onDelete(id);
+      navigate("/", { replace: true });
+    }
+  };
 
   if (!data) {
     return <div>일기를 불러오고 있습니다...</div>;
   } else {
-    return <div>일기 수정 페이지</div>;
+    return (
+      <div>
+        <Header
+          leftChild={<Button text={"< 뒤로 가기"} onClick={goBack} />}
+          title={"일기 수정하기"}
+          rightChild={
+            <Button
+              type={"negative"}
+              text={"삭제하기"}
+              onClick={onClickDelete}
+            />
+          }
+        />
+      </div>
+    );
   }
 };
 
