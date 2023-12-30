d71d2f5d5351e28c2b39b7df8902693b5d411339
sojeong0302 <01duqrl@naver.com>
2023-12-31 05:19:23 +0900

    feat : URL 파라미터로 일기 데이터 가져오기

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom";
+import useDiary from "../hooks/useDiary";
+
 const Edit = () => {
-  return <div className="Edit">Edit 페이지입니다.</div>;
+  const { id } = useParams();
+  const data = useDiary(id);
+
+  if (!data) {
+    return <div>일기를 불러오고 있습니다...</div>;
+  } else {
+    return <div>일기 수정 페이지</div>;
+  }
 };
 
 export default Edit;
